ba99049a1ee0e791ac216bb5276709689e59122b
Maxim Sukhochev <suhochev_m@mail.ru>
2023-01-26 22:56:32 +0300

    Added Control Task

---
+++ ControlTask/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] NewArray(string[] array)                       
+{                                                         
+    string[] findarray = new string[0];                      
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)                             
+        {
+            Array.Resize(ref findarray, findarray.Length + 1);
+            findarray[j] = array[i];                          
+            j++;
+        }
+    }
+    return findarray;
+}
+Console.Write("Вводите элементы массива через запятую и пробел: ");
+string text = Convert.ToString(Console.ReadLine()) ?? String.Empty;
+string[] firstArray = text.Split(", ");
+string[] secondArray = NewArray(firstArray);
+Console.WriteLine();
+Console.WriteLine($"[{String.Join(", ", firstArray)}] -> [{String.Join(", ", secondArray)}]");
+Console.WriteLine();
